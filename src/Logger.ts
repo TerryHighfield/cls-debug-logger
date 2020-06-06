03e2f6113d794bdba4c4c8a20cd8017fe848ffc4
Terry Highfield <t.highfield@gmail.com>
2020-06-07 00:23:08 +0200

    Create log session

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,10 +1,43 @@
 import { ILogProvider } from './ILogProvider';
 import { ILogger } from './ILogger';
+import util from 'util';
+import * as CLS from 'continuation-local-storage';
+import { v4 as uuid } from 'uuid';
 
 export class Logger implements ILogger {
-  constructor(public readonly logProvider: ILogProvider) {}
+  private readonly logSessionIdKey = 'logSessionId';
+
+  constructor(
+    private readonly namespace: string,
+    public readonly logProvider: ILogProvider
+  ) {}
+
+  private getNamespace(): CLS.Namespace {
+    return (
+      CLS.getNamespace(this.namespace) || CLS.createNamespace(this.namespace)
+    );
+  }
+
+  private getSessionId(): string | undefined {
+    const namespace = CLS.getNamespace(this.namespace);
+    return namespace && namespace.get(this.logSessionIdKey);
+  }
 
   public log(message: string | object, ...args: any[]) {
-    this.logProvider.log({ message });
+    this.logProvider.log({
+      message: util.format(message, ...args),
+      session: this.getSessionId() || 'undefined',
+    });
+  }
+
+  public session(session: () => void, sessionId?: string) {
+    const logSessionNS = this.getNamespace();
+    sessionId = sessionId || uuid();
+
+    return logSessionNS.runAndReturn(() => {
+      logSessionNS.set(this.logSessionIdKey, sessionId);
+
+      session();
+    });
   }
 }
--- test/logger.test.ts
+++ test/logger.test.ts
@@ -7,13 +7,125 @@ beforeEach(() => {
   logProvider = TypeMoq.Mock.ofType<ILogProvider>();
 });
 
-test('should log to the log provider', () => {
-  const logger = new Logger(logProvider.object);
+test('should log ILogMessage type to the log provider', () => {
+  const logger = new Logger('testNamespace', logProvider.object);
 
   logger.log('test log');
 
   logProvider.verify(
-    (o) => o.log(TypeMoq.It.isAny()),
+    (o) => o.log(TypeMoq.It.isObjectWith({ message: 'test log' })),
     TypeMoq.Times.exactly(1)
   );
 });
+
+test('should format the log message', () => {
+  const logger = new Logger('namespace', logProvider.object);
+
+  logger.log('%d : %s', 1154, 'a string');
+
+  logProvider.verify(
+    (o) => o.log(TypeMoq.It.isObjectWith({ message: '1154 : a string' })),
+    TypeMoq.Times.exactly(1)
+  );
+});
+
+test('should log a session id', () => {
+  const logger = new Logger('namespace_', logProvider.object);
+
+  logger.session(() => {
+    logger.log('%d : %s', 1154, 'a string');
+  }, 'a test session');
+
+  logProvider.verify(
+    (o) =>
+      o.log(
+        TypeMoq.It.isObjectWith({
+          message: '1154 : a string',
+          session: 'a test session',
+        })
+      ),
+    TypeMoq.Times.exactly(1)
+  );
+});
+
+test("should log 'undefined' for any logs outside a session", () => {
+  const logger = new Logger('namespace_', logProvider.object);
+
+  logger.log('a log message');
+
+  logProvider.verify(
+    (o) =>
+      o.log(
+        TypeMoq.It.isObjectWith({
+          message: 'a log message',
+          session: 'undefined',
+        })
+      ),
+    TypeMoq.Times.exactly(1)
+  );
+});
+
+test('should use a uuid as a default session id', () => {
+  const logger = new Logger('namespace_', logProvider.object);
+
+  logger.session(() => {
+    logger.log('fr');
+  });
+
+  logProvider.verify(
+    (o) =>
+      o.log(
+        TypeMoq.It.is(
+          (log) =>
+            log.message === 'fr' &&
+            log.session !== undefined &&
+            log.session !== 'undefined'
+        )
+      ),
+    TypeMoq.Times.exactly(1)
+  );
+});
+
+test('should use different uuid each time as a default session id', () => {
+  const logger = new Logger('namespace_', logProvider.object);
+
+  logger.session(() => {
+    logger.log('fr');
+  });
+
+  let otherSessionId: string | undefined = undefined;
+  logProvider.verify(
+    (o) =>
+      o.log(
+        TypeMoq.It.is((log) => {
+          otherSessionId = log.session;
+          return (
+            log.message === 'fr' &&
+            log.session !== undefined &&
+            log.session !== 'undefined'
+          );
+        })
+      ),
+    TypeMoq.Times.exactly(1)
+  );
+
+  logger.session(() => {
+    logger.log('fr');
+  });
+
+  logProvider.verify(
+    (o) =>
+      o.log(
+        TypeMoq.It.is(
+          (log) => log.message === 'fr' && log.session !== otherSessionId
+        )
+      ),
+    TypeMoq.Times.exactly(1)
+  );
+});
+
+/*
+TODO:
+ - use default session id
+ - async session
+*/
